0c6e698b03abb066850f5cba4090087e17306a01
Vítor Norberto <vithor.norberto@gmail.com>
2021-01-29 16:08:56 -0300

    Altera email

--- src/main/java/com/acompanhamento/api/config/SpringConfiguration.java
+++ src/main/java/com/acompanhamento/api/config/SpringConfiguration.java
@@ -3,7 +3,6 @@ package com.acompanhamento.api.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
@@ -17,12 +16,12 @@ public class SpringConfiguration {
     }
 
     @Bean
-    public JavaMailSenderImpl javaMailSenderImpl(){
+    public JavaMailSenderImpl javaMailSenderImpl() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("vitor.norberto@academico.fat-al.edu.br");
-        mailSender.setPassword("05353261496");
+        mailSender.setUsername("rafa.tcc.umj@gmail.com");
+        mailSender.setPassword("rafatcc2021");
         Properties prop = mailSender.getJavaMailProperties();
         prop.put("mail.transport.protocol", "smtp");
         prop.put("mail.smtp.auth", "true");
